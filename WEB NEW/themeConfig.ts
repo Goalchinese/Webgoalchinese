4c970a03fe9446af2b2e82d66e1aa8d9a98e1b33
Sunthorn Wisaidee <sunthorn.w@cdg.co.th>
2026-08-15 09:54:54 +0700

    style: update app logo display properties and hide header action

--- WEB NEW/themeConfig.ts
+++ WEB NEW/themeConfig.ts
@@ -11,7 +11,7 @@ import { AppContentLayoutNav, ContentWidth, FooterType, NavbarType } from '@layo
 export const { themeConfig, layoutConfig } = defineThemeConfig({
   app: {
     title: 'goal chinese',
-    logo: h('div', { innerHTML: logo, style: 'line-height:0; color: rgb(var(--v-global-theme-primary))' }),
+    logo: h('div', { innerHTML: logo }),
     contentWidth: ContentWidth.Boxed,
     contentLayoutNav: AppContentLayoutNav.Vertical,
     overlayNavFromBreakpoint: breakpointsVuetify.md + 16, // 16 for scrollbar. Docs: https://next.vuetifyjs.com/en/features/display-and-platform/
